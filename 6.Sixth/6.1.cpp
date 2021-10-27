c907aebe652570cbeaea6e30d21835871d940233
Velovatik <velovatiykirill@gmail.com>
2021-10-27 18:56:15 +0300

    Added sixth lecture

---
+++ 6.Sixth/6.1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <exception>
+using namespace std;
+
+void it(vector<int> vect, int index)
+{
+    cout << vect.at(index);
+    return it(vect, index + 1);
+}
+
+int main()
+{
+    try
+    {
+        vector<int> vect(5);
+        int i = 2;
+        generate(vect.begin(), vect.end(), [&i] {i *= 2; return i; });
+        it(vect, 0);
+    }
+    catch (exception& exception)
+    {
+        cout << "\n" << "Done";
+    }
+
+
+    return 0;
+}
